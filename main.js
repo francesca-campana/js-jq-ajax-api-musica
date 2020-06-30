3bb849500207825467daff8d43f91d327e98111e
francesca-campana <franx3rox@gmail.com>
2020-06-30 18:32:05 +0200

    revisione

--- main.js
+++ main.js
@@ -7,61 +7,45 @@ $(document).ready(function() {
       url: 'https://flynn.boolean.careers/exercises/api/array/music',
       method: 'GET',
       success: function(data){
-
         var cds = data.response;
         console.log(cds)
         printCd(cds);
-        
-        $('.generi').change(function(){
-
-          var selectGenere = $(this).val();
-          if (selectGenere === '') {
-            $('.cds').fadeIn();
-
-          }else {
-            $('.cd').each(function(){
-              var singolGenere = $(this).attr('data-genere');
-              if (singolGenere.toLowerCase() === selectGenere.toLowerCase()) {
-                console.log(this);
-                $(this).fadeIn();
-              }else {
-                console.log(this);
-
-                $(this).fadeOut();
-              }
-
-            });
-
-          }
-
-
-
-
-        });
-
       },
 
       error: function(){
         alert('errore');
       }
+
+    });
+//Utilizzo una select per filtrare i dischi per genere musicale e mostrare solo quelli selezionati
+  $('.generi').change(function(){
+
+    var selectGenere = $(this).val();
+      if (selectGenere == 'generi') {
+        $('.cd').fadeIn();
+        console.log(selectGenere);
+      }else {
+        $('.cd').fadeOut();
+        $('.cd.' + selectGenere).fadeIn();
+      }
     });
 
 
 //Stampo i cd a schermo attraverso una funzione che utilizzerò all'interno della chiamata ajax
-    function printCd(listaCd){
+  function printCd(listaCd){
 
-      var source = $('#cds-template').html();
-      var template = Handlebars.compile(source);
+    var source = $('#cds-template').html();
+    var template = Handlebars.compile(source);
 
-      for (var i = 0; i < listaCd.length; i++) {
-        var singoloCd = listaCd[i];
+    for (var i = 0; i < listaCd.length; i++) {
+      var singoloCd = listaCd[i];
+      singoloCd.genre = singoloCd.genre.toLowerCase();
 
-        var html = template(singoloCd);
-        $('.cds-container').append(html);
-      }
+
+      var html = template(singoloCd);
+      $('.cds-container').append(html);
     }
-//Utilizzo una select per filtrare i dischi per genere musicale e mostrare solo quelli selezionati
-
+  }
 
 
 });
